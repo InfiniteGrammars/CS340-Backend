c1f920b884e55a0f5f30fc6e75732ea0be7f2221
Alex Hollinghead <alexhollinghead@pm.me>
2023-08-01 22:59:42 -0400

    Create Node server and first route

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # CS340-Project
+
+## Requirements
+Node v.18.17
+
+## Getting Started
+If you are running the server for the first time, first run:
+`npm install`
+
+To start the server:
+`node index.js`
+
+Routes can be accessed locally at `localhost:3000`
+
+## Routes
+
+### get-posts
+Returns a list of all posts
--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Hello, world!');
+import express from "express";
+
+// Express Server Config
+const app = express();
+app.use(express.json());
+
+
+app.get('/get-posts', (req, res) => {
+  /* Insert SQL query here, e.g.:
+     getPosts = 'SELECT * FROM Posts'; */
+
+
+  /* Execute query on DB:
+    db.pool.query(getPosts, function(err, results, fields){
+     // Send results to browser:
+    res.send(JSON.stringify(results));
+  }) */
+
+  // This is just for testing while there are no queries:
+  res.send(`Route 'get-posts' is active.`);
+})
+
+app.listen(3000, ()=> {
+  console.log(`Server started...`);
+})
