fc02475ec8f46c28fa15f2073dcc9a07d5497098
Alex Hollinghead <alexhollinghead@pm.me>
2023-08-03 13:31:08 -0400

    Fix minor details and add error handling for Posts routes

--- README.md
+++ README.md
@@ -1,18 +1,21 @@
 # CS340-Project
 
 ## Requirements
+
 Node v.18.13
 
 ## Getting Started
+
 If you are running the server for the first time, first run:
 `npm install`
 
 To start the server:
-`node index.js`
+`npm start`
 
 Routes can be accessed locally at `localhost:3000`
 
 ## Routes
 
 ### get-posts
+
 Returns a list of all posts
--- index.js
+++ index.js
@@ -7,85 +7,116 @@ app.use(express.json());
 const db = require('./database/db-connector');
 
 //main page
-app.get('/', (req, res) => { 
+app.get('/', (req, res) => {
   res.send('Bird App Administration'); //placeholder
 });
 
-/* queries for posts */ 
+/* queries for posts */
 
 app.get('/get-posts', (req, res) => {
+
   //sql query to get all posts in the database and return them
+  const getPosts = 'select * from Posts;';
 
-  let getPosts = 'select * from Posts;'; 
-
-  db.pool.query(getPosts, function(err, results, fields) { 
-    res.send(JSON.stringify(results));
-  });
- 
-});
-
-app.get('/get-posts-by-user', (req, res) => { 
-
-  //returns posts by a specific user (chosen from dropdown by username) 
-  let getPostsByUser = 'select * from Posts ' 
-  + 'inner join Users on Posts.user_id = Users.user_id ' 
-  + `where Users.username = ${req.query.user_id};`;
-
-  db.pool.query(getPostsByUser, function(err, results, fields) { 
+  db.pool.query(getPosts, function (err, results, fields) {
     res.send(JSON.stringify(results));
   });
 });
 
-app.get('get-posts-by-group', (req, res) => {
+app.get('/get-posts-by-user', (req, res) => {
 
-  //show all posts in a given group 
-  let getPostsByGroup = 'select * from Posts ' 
-  + `where group_id = ${req.query.group_id};`;
+  //returns posts by a specific user (chosen from dropdown by username)
+  const getPostsByUser = `
+    select * from Posts
+    inner join Users on Posts.user_id = Users.user_id
+    where Users.user_id = ${req.query.user_id};`;
+
+  db.pool.query(getPostsByUser, function (err, results, fields) {
+    if (err) {
+      res.status(500).send(
+        `There was an error fetching posts for user ${req.query.user_id}: ${err}`
+      );
+    } else {
+      res.status(200).send(JSON.stringify(results));
+    }
+  });
+});
+
+app.get('/get-posts-by-group', (req, res) => {
+
+  //show all posts in a given group
+  const getPostsByGroup = `
+    select * from Posts
+    where group_posted = ${req.query.group_id};`;
 
   db.pool.query(getPostsByGroup, function (err, results, fields) {
-    res.send(JSON.stringify(results));
+    if (err) {
+      res.status(500).send(
+        `There was an error fetching posts for group ${req.query.group_id}: ${err}`
+      );
+    } else {
+      res.status(200).send(JSON.stringify(results));
+    }
   });
 });
 
-app.get('/create-post', (req, res) => { 
-  
-  //allows creation of new post 
-  let createPost = 'insert into Posts (user_id, post_body, time_posted, group_posted) ' 
-  + `values (${req.query.user_id}, 
-            ${req.query.post_body}, 
-            ${req.query.time_posted}, 
-            ${req.query.group_posted});`;
-  
-  db.pool.query(createPost, function (err, results, fields) {
-    res.send('Post created.')
-  });
+app.post('/create-post', (req, res) => {
 
+  //allows creation of new post
+  const createPost = 'INSERT INTO Posts (user_id, post_body, time_posted, group_posted) VALUES (?, ?, ?, ?)';
+  const data = [req.body.user_id, req.body.post_body, req.body.time_posted, req.body.group_posted];
+
+  console.log(req.body);
+
+  db.pool.query(createPost, data, (err, results, fields) => {
+    if (err) {
+      res.status(500).send(
+        `There was an error creating the post: ${err}`
+      );
+    } else {
+      res.status(200).send(JSON.stringify(results));
+    }
+  });
 });
 
 app.get('/delete-post', (req, res) => {
 
   //deletes a given post based on post_id
-  let deletePost = 'delete from Posts '
-  + `where post_id = ${req.query.post_id};`; 
+  const deletePost = `
+    delete from Posts
+    where post_id = ${req.query.post_id};`;
 
-  db.pool.query(deletePost, function (err, results, fields) {
-    res.send('Post deleted.');
+  db.pool.query(deletePost, (err, results, fields) => {
+    if (err) {
+      res.status(500).send(
+        `There was an error deleting post ${req.query.post_id}: ${err}`
+      );
+    } else {
+      res.send('Post deleted.');
+    }
   });
 });
 
-app.get('/update-post-group', (req, res) => { 
+app.get('/update-post-group', (req, res) => {
 
   //updates which group a post was made in (or makes it public)
-  let updatePostGroup = 'update Posts '
-  + `set group_id = ${req.query.group_id} ` 
-  + `where post_id = ${req.query.post_id};`;
+  const updatePostGroup = `
+    update Posts
+    set group_posted = ${req.query.group_id}
+    where post_id = ${req.query.post_id};`;
 
-  db.pool.query(updatePostGroup, function (err, results, fields) { 
-    res.send('Group updated.');
+  db.pool.query(updatePostGroup, function (err, results, fields) {
+    if (err) {
+      res.status(500).send(
+        `There was an error updating group for post ${req.query.post_id}: ${err}`
+      );
+    } else {
+      res.status(200).send('Group updated.');
+    }
   });
 });
 
 
-app.listen(3000, ()=> {
-  console.log(`Server started...`);
-})
+app.listen(3000, () => {
+  console.log(`Server started. Listening on port 3000...`)
+});
