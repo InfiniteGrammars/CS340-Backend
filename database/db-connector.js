6b0906b9c4bb9f3d82579956efcce9dc6a2e2a09
Alex Hollinghead <alexhollinghead@pm.me>
2023-08-14 22:45:56 -0400

    Sanitzied dbconnector

--- database/db-connector.js
+++ database/db-connector.js
@@ -1,14 +1,20 @@
+/*
+Date: 08/03/2023
+Based on:
+Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main/Step%201%20-%20Connecting%20to%20a%20MySQL%20Database
+*/
+
 //create a connection pool and then export it so we can use it in the rest of our files
 
-let mysql = require('mysql'); 
+let mysql = require("mysql");
+require("dotenv").config(); //save passwords in separate private file
 
 let pool = mysql.createPool({
-   connectionLimit : 10,
-   host : 'classmysql.engr.oregonstate.edu',
-   user : 'cs340_escotoso',
-   password : '7868',
-   database : 'cs340_escotoso'
+   connectionLimit: 10,
+   host: process.env.DB_HOST,
+   user: process.env.DB_USER,
+   password: process.env.DB_PASSWORD,
+   database: process.env.DB,
 });
 
 module.exports.pool = pool;
-
